69d926efa02f381c45a99a47c35d1bb066660eb5
NatthawutKunphom <108015365+NatthawutKunphom@users.noreply.github.com>
2022-08-12 17:04:44 +0700

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,10 +3,11 @@
 
 $name = $_POST['name'];
 $email= $_POST['email'];
+$message= $_POST['subject'];
 $message= $_POST['message'];
 $to = "natthawut.ku.2002@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$txt ="Name = ". $name . "\r\n  Email = " . $email ."\r\n Subject =" . $message. "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
